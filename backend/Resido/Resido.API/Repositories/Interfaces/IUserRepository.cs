e90d703f2237c0855d8114fbd6a525688820ffdf
hindeptrai <hientm0978@gmail.com>
2026-05-02 13:53:22 +0700

    Refractor code query db từ service sang repository

---
+++ backend/Resido/Resido.API/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Resido.API.Models;
+
+namespace Resido.API.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<User?> GetByUsernameAsync(string username);
+    }
+}
---
+++ backend/Resido/Resido.API/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Resido.API.Models;
+using Resido.API.Repositories.Interfaces;
+
+namespace Resido.API.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ResidoDbContext _dbContext;
+        public UserRepository(ResidoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<User?> GetByUsernameAsync(string username)
+        {
+            return await _dbContext.Users
+                .FirstOrDefaultAsync(u => u.Username == username);
+        }
+    }
+}
