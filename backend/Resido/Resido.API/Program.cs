8f04ac2b99daae1990d07c250ed92729bc157f15
hindeptrai <hientm0978@gmail.com>
2026-05-02 12:56:59 +0700

    Thêm cấu hình lấy chuỗi kết nối từ file appsetting

--- backend/Resido/Resido.API/Program.cs
+++ backend/Resido/Resido.API/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Resido.API.Models;
+
 namespace Resido.API
 {
     public class Program
@@ -13,6 +16,12 @@ namespace Resido.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            
+
+            builder.Services.AddDbContext<ResidoDbContext>(options =>
+                options.UseSqlServer(builder.Configuration
+                    .GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
