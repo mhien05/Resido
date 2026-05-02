ccd7a3eafdc1600a455327fdb0dfe57db938e808
hindeptrai <hientm0978@gmail.com>
2026-05-02 13:34:22 +0700

    Thêm khung request và response

---
+++ backend/Resido/Resido.API/DTOs/Responses/LoginResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Resido.API.DTOs.Responses
+{
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public DateTime ExpiresAt { get; set; }
+    }
+}
