6833521f93288dc199a33db65a5ce3565a251486
hindeptrai <hientm0978@gmail.com>
2026-05-02 12:55:30 +0700

    fix: Thay đổi property trong model property

--- backend/Resido/Resido.API/Models/Property.cs
+++ backend/Resido/Resido.API/Models/Property.cs
@@ -2,7 +2,7 @@
 {
     public class Property
     {
-        public Guid id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string? Description { get; set; }
