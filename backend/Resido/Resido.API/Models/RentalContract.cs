15dbe3f7917e632710648f935c8717235f78a153
hindeptrai <hientm0978@gmail.com>
2026-05-02 13:01:47 +0700

    Khởi tạo model RetalContract

---
+++ backend/Resido/Resido.API/Models/RentalContract.cs
@@ -0,0 +1,11 @@
+﻿namespace Resido.API.Models
+{
+    public class RentalContract
+    {
+        public Guid Id { get; set; }
+        public Guid RoomId { get; set; }
+
+        // Navigation
+        public Room Room { get; set; } = null!;
+    }
+}
