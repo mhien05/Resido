4123722e06e07689202f1e6604090e478907e067
hindeptrai <hientm0978@gmail.com>
2026-05-02 13:34:03 +0700

    Khởi tạo authController xử lý request đăng nhập

---
+++ backend/Resido/Resido.API/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Resido.API.DTOs.Requests;
+using Resido.API.Models;
+using Resido.API.Services.Interfaces;
+
+namespace Resido.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var response = await _authService.LoginAsync(request);
+
+            if (response == null)
+                return Unauthorized(new { message = "Username hoặc password không đúng" });
+
+            return Ok(response);
+        }
+    }
+}
