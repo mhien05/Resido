c7af7df13ea3504a3b05ae682af289484d64b653
hindeptrai <hientm0978@gmail.com>
2026-05-03 13:43:28 +0700

    feat: Thêm DTO response Room

---
+++ backend/Resido/Resido.API/DTOs/Responses/RoomResponse.cs
@@ -0,0 +1,20 @@
+﻿using Resido.API.Enums;
+using System.Data.SqlTypes;
+
+namespace Resido.API.DTOs.Responses
+{
+    public class RoomResponse
+    {
+        public Guid Id { get; set; }
+        public Guid PropertyId { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public int Floor { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public decimal RentPrice { get; set; }
+        public decimal ElectricPrice { get; set; }
+        public decimal WaterPrice { get; set; }
+        public decimal ServiceFee { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } = null;
+    }
+}
