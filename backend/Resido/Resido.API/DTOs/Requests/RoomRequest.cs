5a7bdc68face60280d7a2f2ef9a1ae283f4d5010
hindeptrai <hientm0978@gmail.com>
2026-05-03 13:42:39 +0700

    feat: Thêm DTO request Room

---
+++ backend/Resido/Resido.API/DTOs/Requests/RoomRequest.cs
@@ -0,0 +1,16 @@
+﻿using Resido.API.Enums;
+
+namespace Resido.API.DTOs.Requests
+{
+    public class RoomRequest
+    {
+        public Guid PropertyId { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public int Floor { get; set; }
+        public RoomStatus Status { get; set; } = RoomStatus.Available;
+        public decimal RentPrice { get; set; }
+        public decimal ElectricPrice { get; set; }
+        public decimal WaterPrice { get; set; }
+        public decimal ServiceFee { get; set; }
+    }
+}
