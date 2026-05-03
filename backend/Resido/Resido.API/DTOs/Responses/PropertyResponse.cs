6234c170d958f6caf43beb64a76d6e124c23116a
hindeptrai <hientm0978@gmail.com>
2026-05-03 13:43:10 +0700

    feat: Thêm DTO response Property

---
+++ backend/Resido/Resido.API/DTOs/Responses/PropertyResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Resido.API.DTOs.Responses
+{
+    public class PropertyResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } = null;
+    }
+}
