01229e78c13287dde0ea8dc4c2c96111585a1cff
hindeptrai <hientm0978@gmail.com>
2026-05-03 14:52:00 +0700

    chore: Tạo thư mục implement

---
+++ backend/Resido/Resido.API/Repositories/Implements/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Resido.API.Models;
+using Resido.API.Repositories.Interfaces;
+
+namespace Resido.API.Repositories.Implements
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ResidoDbContext _dbContext;
+        public UserRepository(ResidoDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<User?> GetByUsernameAsync(string username)
+        {
+            return await _dbContext.Users
+                .FirstOrDefaultAsync(u => u.Username == username
+                                            && u.IsActive);
+        }
+    }
+}
