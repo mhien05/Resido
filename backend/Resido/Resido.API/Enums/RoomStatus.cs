01dead93b5529bfe2b4ef93096a457f1a637e9d2
hindeptrai <hientm0978@gmail.com>
2026-05-03 13:14:44 +0700

    feat: Thêm file cấu hình status của Rooms

--- backend/Resido/Resido.API/Enums/RoomStatus.cs
+++ backend/Resido/Resido.API/Enums/RoomStatus.cs
@@ -1,6 +1,9 @@
 ﻿namespace Resido.API.Enums
 {
-    public class RoomStatus
+    public enum RoomStatus
     {
+        Available = 1, // Có sẵn
+        Occupied = 2, // Đã có người sử dụng
+        Maintenance = 3 // Bảo trì
     }
 }
