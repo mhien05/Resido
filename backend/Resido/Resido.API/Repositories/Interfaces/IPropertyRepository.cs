10167b99f412864c78ca3a0e59939e9c606a10ee
hindeptrai <hientm0978@gmail.com>
2026-05-03 14:52:40 +0700

    feat: Tạo file định nghĩa các method Property

---
+++ backend/Resido/Resido.API/Repositories/Interfaces/IPropertyRepository.cs
@@ -0,0 +1,32 @@
+﻿using Resido.API.Models;
+
+namespace Resido.API.Repositories.Interfaces
+{
+    public interface IPropertyRepository
+    {
+        /// <summary>
+        /// Lấy toàn bộ thông tin Property
+        /// </summary>
+        /// <returns>
+        /// IEnumerable với Property Object
+        /// </returns>
+        public Task<IEnumerable<Property>> GetAllAsync();
+        /// <summary>
+        /// Lấy thông tin Property bằng id
+        /// </summary>
+        public Task<Property?> GetByIdAsync(Guid id);
+        /// <summary>
+        /// Thêm property
+        /// </summary>
+        public Task AddAsync(Property property);
+        /// <summary>
+        /// Cập nhật thông tin Property
+        /// </summary>
+        public Task UpdateAsync(Property property);
+        /// <summary>
+        /// Xóa (soft delete) Property
+        /// </summary>
+        public Task DeleteAsync(Property property);
+
+    }
+}
