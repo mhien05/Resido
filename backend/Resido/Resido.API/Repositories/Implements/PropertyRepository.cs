bd790c5fef97c5a8d98e65dd0df2d4c84a04ee84
hindeptrai <hientm0978@gmail.com>
2026-05-03 15:17:41 +0700

    feat: Implement interface IPropertyRepository

---
+++ backend/Resido/Resido.API/Repositories/Implements/PropertyRepository.cs
@@ -0,0 +1,33 @@
+﻿using Resido.API.Models;
+using Resido.API.Repositories.Interfaces;
+
+namespace Resido.API.Repositories.Implements
+{
+    public class PropertyRepository : IPropertyRepository
+    {
+        public Task AddAsync(Property property)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Property property)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Property>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Property?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Property property)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
