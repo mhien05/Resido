0186cce140126a7cde728f21661d3832f173a85b
hindeptrai <hientm0978@gmail.com>
2026-05-03 13:27:42 +0700

    migrate: Migration thay đổi của DbContext và Room Entities

--- backend/Resido/Resido.API/Migrations/20260503062601_UpdateRoomFields.Designer.cs
+++ backend/Resido/Resido.API/Migrations/20260503062601_UpdateRoomFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Resido.API.Models;
 
@@ -11,9 +12,11 @@ using Resido.API.Models;
 namespace Resido.API.Migrations
 {
     [DbContext(typeof(ResidoDbContext))]
-    partial class ResidoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260503062601_UpdateRoomFields")]
+    partial class UpdateRoomFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/Resido/Resido.API/Migrations/20260503062601_UpdateRoomFields.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Resido.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRoomFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Rooms",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Rooms",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Rooms",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Rooms");
+        }
+    }
+}
