7f501a64f126981db9c4c878e81394cc586427b2
hindeptrai <hientm0978@gmail.com>
2026-05-03 15:10:25 +0700

    fix: thay đổi tham số nhận vào của các method

--- backend/Resido/Resido.API/Repositories/Implements/RoomRepository.cs
+++ backend/Resido/Resido.API/Repositories/Implements/RoomRepository.cs
@@ -6,44 +6,44 @@ namespace Resido.API.Repositories.Implements
 {
     public class RoomRepository : IRoomRepository
     {
-        public readonly ResidoDbContext _dbContext;
+        private readonly ResidoDbContext _dbContext;
         public RoomRepository(ResidoDbContext dbContext) 
         {
             _dbContext = dbContext;
         }
 
-        public async Task AddAsync(Property property)
+        public async Task AddAsync(Room room)
         {
-            await _dbContext.AddAsync(property);
+            await _dbContext.AddAsync(room);
             await _dbContext.SaveChangesAsync();
         }
 
-        public Task DeleteAsync(Property property)
+        public Task DeleteAsync(Room room)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Property>> GetAllASync()
+        public Task<IEnumerable<Room>> GetAllAsync()
         {
             throw new NotImplementedException();
         }
 
-        public Task<Property?> GetByIdAsync(Guid id)
+        public Task<Room?> GetByIdAsync(Guid id)
         {
             throw new NotImplementedException();
         }
 
-        public Task GetByPropertyIdAsync(Guid id)
+        public Task<IEnumerable<Room>> GetByPropertyIdAsync(Guid propertyId)
         {
             throw new NotImplementedException();
         }
 
-        public Task GetByStatusAsync(RoomStatus status)
+        public Task<IEnumerable<Room>> GetByStatusAsync(RoomStatus status)
         {
             throw new NotImplementedException();
         }
 
-        public Task UpdateAsync(Property property)
+        public Task UpdateAsync(Room room)
         {
             throw new NotImplementedException();
         }
