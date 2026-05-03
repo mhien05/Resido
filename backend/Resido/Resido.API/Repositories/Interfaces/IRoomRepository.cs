5d74b9e27b9328173e582abddd97d8bd21c7f60a
hindeptrai <hientm0978@gmail.com>
2026-05-03 15:10:58 +0700

    fix: Sửa tham số & type tên method

--- backend/Resido/Resido.API/Repositories/Interfaces/IRoomRepository.cs
+++ backend/Resido/Resido.API/Repositories/Interfaces/IRoomRepository.cs
@@ -10,30 +10,30 @@ namespace Resido.API.Repositories.Interfaces
         /// <summary>
         /// Lấy toàn bộ thông tin Rooms
         /// </summary>
-        public Task<IEnumerable<Property>> GetAllASync();
+        public Task<IEnumerable<Room>> GetAllAsync();
         /// <summary>
         /// Lấy thông tin room theo ID
         /// </summary>
-        public Task<Property?> GetByIdAsync(Guid id);
+        public Task<Room?> GetByIdAsync(Guid id);
         /// <summary>
         /// Thêm thông tin room
         /// </summary>
-        public Task AddAsync(Property property);
+        public Task AddAsync(Room room);
         /// <summary>
         /// Cập nhật thông tin room
         /// </summary>
-        public Task UpdateAsync(Property property);
+        public Task UpdateAsync(Room room);
         /// <summary>
         /// Xóa (soft delete) thông tin room
         /// </summary>
-        public Task DeleteAsync(Property property);
+        public Task DeleteAsync(Room room);
         /// <summary>
         /// Lấy thông tin phòng theo Property ID
         /// </summary>
-        public Task GetByPropertyIdAsync(Guid id);
+        public Task<IEnumerable<Room>> GetByPropertyIdAsync(Guid propertyId);
         /// <summary>
         /// Lấy thông tin phòng theo status
         /// </summary>
-        public Task GetByStatusAsync(RoomStatus status);
+        public Task<IEnumerable<Room>> GetByStatusAsync(RoomStatus status);
     }
 }
