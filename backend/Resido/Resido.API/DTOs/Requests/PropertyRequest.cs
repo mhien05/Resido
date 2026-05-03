ab73093014aaa4501c81f6b7bc17f216a6d570c8
hindeptrai <hientm0978@gmail.com>
2026-05-03 13:42:25 +0700

    feat: Thêm DTO request Property

---
+++ backend/Resido/Resido.API/DTOs/Requests/PropertyRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Resido.API.DTOs.Requests
+{
+    public class PropertyRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string? Description { get; set; } = null;
+    }
+}
