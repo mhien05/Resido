49367c87002025915b8fed86eccd4ac4f84a79e7
hindeptrai <hientm0978@gmail.com>
2026-05-04 12:38:02 +0700

    feat: Thêm interface service của Property

---
+++ backend/Resido/Resido.API/Services/Interfaces/IPropertyService.cs
@@ -0,0 +1,14 @@
+﻿using Resido.API.DTOs.Requests;
+using Resido.API.DTOs.Responses;
+
+namespace Resido.API.Services.Interfaces
+{
+    public interface IPropertyService
+    {
+        public Task<IEnumerable<PropertyResponse>> GetAllAsync();
+        public Task<PropertyResponse?> GetByIdAsync(Guid id);
+        public Task CreateAsync(PropertyRequest request);
+        public Task UpdateAsync(Guid id, PropertyRequest request);
+        public Task DeleteAsync(Guid id);
+    }
+}
