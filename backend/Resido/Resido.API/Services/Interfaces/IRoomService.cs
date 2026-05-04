fddca0c9aad6e83101297c02e65d83376335ea97
hindeptrai <hientm0978@gmail.com>
2026-05-04 12:38:19 +0700

    feat: Thêm interface service của Rooms

---
+++ backend/Resido/Resido.API/Services/Interfaces/IRoomService.cs
@@ -0,0 +1,18 @@
+﻿using Resido.API.DTOs.Requests;
+using Resido.API.DTOs.Responses;
+using Resido.API.Repositories.Implements;
+
+namespace Resido.API.Services.Interfaces
+{
+    public interface IRoomService
+    {
+        public Task<IEnumerable<RoomResponse>> GetAllAsync();
+        public Task<RoomResponse?> GetByIdAsync(Guid id);
+        public Task CreateAsync(RoomRequest request);
+        public Task UpdateAsync(Guid id, RoomRequest request);
+        public Task DeleteAsync(Guid id);
+
+        public Task<IEnumerable<RoomResponse>> GetByPropertyIdAsync(Guid propertyId);
+        public Task<IEnumerable<RoomResponse>> GetByStatusAsync(string status);
+    }
+}
