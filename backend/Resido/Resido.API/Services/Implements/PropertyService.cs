92ad1f2939ff15fe63d8ec0502219f9e95dbd618
hindeptrai <hientm0978@gmail.com>
2026-05-04 22:13:35 +0700

    feat: implement các method

--- backend/Resido/Resido.API/Services/Implements/PropertyService.cs
+++ backend/Resido/Resido.API/Services/Implements/PropertyService.cs
@@ -1,5 +1,7 @@
-﻿using Resido.API.DTOs.Requests;
+﻿using Microsoft.VisualBasic;
+using Resido.API.DTOs.Requests;
 using Resido.API.DTOs.Responses;
+using Resido.API.Models;
 using Resido.API.Repositories.Interfaces;
 using Resido.API.Services.Interfaces;
 
@@ -14,29 +16,94 @@ namespace Resido.API.Services.Implements
             _propertyRepository = propertyRepository;
         }
 
-        public Task CreateAsync(PropertyRequest request)
+        public async Task CreateAsync(PropertyRequest request)
         {
-            throw new NotImplementedException();
+            // Tạo entity mới từ Request
+            var property = new Property
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Address = request.Address,
+                Description = request.Description
+            };
+
+            await _propertyRepository.AddAsync(property);
         }
 
-        public Task DeleteAsync(Guid id)
+        public async Task DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            // Gọi method GetById ở repository
+            var property = await _propertyRepository.GetByIdAsync(id);
+
+            // Kiểm tra null
+            if(property == null)
+                throw new KeyNotFoundException($"Property với ID {id} không tồn tại");
+
+            // Gọi method Delete ở repository
+            await _propertyRepository.DeleteAsync(property);
         }
 
-        public Task<IEnumerable<PropertyResponse>> GetAllAsync()
+        public async Task<IEnumerable<PropertyResponse>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            // Gọi repository để lấy danh sách Entity
+            var properties = await _propertyRepository.GetAllAsync();
+
+            // Map từ property (Entity) -> PropertyResponse (DTO)
+            var response = properties.Select(p => new PropertyResponse
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Address = p.Address,
+                Description = p.Description,
+                IsActive = p.IsActive,
+                CreatedAt = p.CreatedAt,
+                UpdatedAt = p.UpdatedAt
+            });
+
+            return response;
         }
 
-        public Task<PropertyResponse?> GetByIdAsync(Guid id)
+
+        public async Task<PropertyResponse?> GetByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            // Gọi repository
+            var property = await _propertyRepository.GetByIdAsync(id);
+
+            // Nếu không tìm thấy return null
+            if(property == null) return null;
+
+            // Map Entity -> Response
+            var response = new PropertyResponse
+            {
+                Id = property.Id,
+                Name = property.Name,
+                Address = property.Address,
+                Description = property.Description,
+                IsActive = property.IsActive,
+                CreatedAt = property.CreatedAt,
+                UpdatedAt = property.UpdatedAt
+            };
+
+            // Return
+            return response;
         }
 
-        public Task UpdateAsync(Guid id, PropertyRequest request)
+        public async Task UpdateAsync(Guid id, PropertyRequest request)
         {
-            throw new NotImplementedException();
+            // Gọi method GetById ở repository
+            var property = await _propertyRepository.GetByIdAsync(id);
+
+            // Kiểm tra entity có tồn tại không 
+            if(property == null)
+                throw new KeyNotFoundException($"Property với ID {id} không tồn tại");
+
+            // Cập nhật các field từ request
+            property.Name = request.Name;
+            property.Address = request.Address;
+            property.Description = request.Description;
+
+            // Gọi method Update ở repository
+            await _propertyRepository.UpdateAsync(property);
         }
     }
 }
