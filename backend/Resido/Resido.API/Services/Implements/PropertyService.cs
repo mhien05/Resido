77f139ae275b481dde4d5c80c56334e5e613079c
hindeptrai <hientm0978@gmail.com>
2026-05-04 12:37:39 +0700

    feat: Implement interface service của Property

---
+++ backend/Resido/Resido.API/Services/Implements/PropertyService.cs
@@ -0,0 +1,42 @@
+﻿using Resido.API.DTOs.Requests;
+using Resido.API.DTOs.Responses;
+using Resido.API.Repositories.Interfaces;
+using Resido.API.Services.Interfaces;
+
+namespace Resido.API.Services.Implements
+{
+    public class PropertyService : IPropertyService
+    {
+        private readonly IPropertyRepository _propertyRepository;
+
+        public PropertyService(IPropertyRepository propertyRepository)
+        {
+            _propertyRepository = propertyRepository;
+        }
+
+        public Task CreateAsync(PropertyRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PropertyResponse>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PropertyResponse?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Guid id, PropertyRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
