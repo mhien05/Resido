7ff502fb341fe69ecaa187d9046f49891e01ada9
hindeptrai <hientm0978@gmail.com>
2026-05-08 19:08:40 +0700

    feat: thêm api lấy toàn bộ thông tin các phòng

---
+++ backend/Resido/Resido.API/Controllers/RoomController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resido.API.Services.Interfaces;
+
+namespace Resido.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+
+        public RoomController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        [HttpGet("room")]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _roomService.GetAllAsync();
+
+            return Ok(response);
+        }
+    }
+}
