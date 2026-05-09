46e9eb6fc9db8e91f2ffa44888d9e6e2c756c5f4
hindeptrai <hientm0978@gmail.com>
2026-05-09 15:33:13 +0700

    feat: thêm xong các api còn thiếu của room
    - Update thông tin phòng
    - Tìm phòng bằng property id
    - Tìm phòng bằng status

--- backend/Resido/Resido.API/Controllers/RoomController.cs
+++ backend/Resido/Resido.API/Controllers/RoomController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Resido.API.DTOs.Requests;
+using Resido.API.Enums;
 using Resido.API.Repositories.Implements;
 using Resido.API.Services.Interfaces;
 using System.Runtime.CompilerServices;
@@ -18,6 +19,7 @@ namespace Resido.API.Controllers
             _roomService = roomService;
         }
 
+        // Get all
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
@@ -26,6 +28,7 @@ namespace Resido.API.Controllers
             return Ok(response);
         }
 
+        // Get by id
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(Guid id)
         {
@@ -34,12 +37,49 @@ namespace Resido.API.Controllers
             return Ok(response);
         }
 
+        // Create
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] RoomRequest roomRequest)
         {
             await _roomService.CreateAsync(roomRequest);
 
             return StatusCode(201, new { message = "Tạo phòng thành công" });
+        }
+
+        // Update
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] RoomRequest roomRequest)
+        {
+            await _roomService.UpdateAsync(id, roomRequest);
+
+            return StatusCode(200, new { message = "Cập nhật thông tin phòng thành công" });
+        }
+
+        // Delete
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _roomService.DeleteAsync(id);
+
+            return StatusCode(204, new { message = $"Xóa phòng có id {id} thành công" });
+        }
+
+        // Get by status
+        [HttpGet("by-property/{propertyId}")]
+        public async Task<IActionResult> GetByPropertyId(Guid propertyId)
+        {
+            var response = await _roomService.GetByPropertyIdAsync(propertyId);
+
+            return Ok(response);
+        }
+
+        // Get by property id
+        [HttpGet("by-status/{status}")]
+        public async Task<IActionResult> GetByStatus(RoomStatus status)
+        {
+            var response = await _roomService.GetByStatusAsync(status);
+
+            return Ok(response);
         }
     }
 }
