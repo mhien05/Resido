6f7beccc1da71f8923ebbb08e3503eaf9877dd40
hindeptrai <hientm0978@gmail.com>
2026-05-11 14:00:36 +0700

    feat: add middleware handle exception

---
+++ backend/Resido/Resido.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Resido.API.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next; 
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            context.Response.StatusCode = 404;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsJsonAsync(new { message = ex.Message });
+        }
+        catch (InvalidOperationException ex)
+        {
+            context.Response.StatusCode = 400;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsJsonAsync(new { message = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = 500;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsJsonAsync(new { message = "Lỗi server" });
+        }
+    }
+}
