f125fe5e80a5f9ace4b463e0faa51466e55d5a0d
hindeptrai <hientm0978@gmail.com>
2026-05-11 12:25:01 +0700

    feat: thêm file controller api property

---
+++ backend/Resido/Resido.API/Controllers/PropertyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Resido.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PropertyController : ControllerBase
+    {
+
+    }
+}
