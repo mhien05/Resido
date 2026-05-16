207e270f28cf412befff5e03b312c24823ceb875
hindeptrai <hientm0978@gmail.com>
2026-05-17 06:52:19 +0700

    feat(room): add room response message constant

---
+++ backend/Resido/Resido.API/Constants/MessageConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Resido.API.Constants;
+
+public static class MessageConstants
+{
+    public static class Room
+    {
+        public const string Created = "Tạo phòng thành công";
+        public const string Updated = "";
+        public const string Deleted = "";
+        public const string NotFound = "";
+    }
+}
